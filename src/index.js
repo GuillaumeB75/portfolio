cf2d5614a4257ee03d6b0d226ce4701f6ce9703f
GuillaumeB75 <guillaume.bezie@yahoo.com>
2024-02-01 22:39:13 +0100

    router en place

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <Router></Router>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
