57114a6af0ec13f08b6b540bb276401bb92ebf0a
GuillaumeB75 <guillaume.bezie@yahoo.com>
2024-02-02 15:45:31 +0100

    mise en forme avec prettier

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-    <RoutesDefinition />
+      <RoutesDefinition />
     </Router>
   </React.StrictMode>
 );
