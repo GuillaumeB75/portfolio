ad9dedd7a27c95c5940f33d7a71520cd78f11be5
GuillaumeB75 <guillaume.bezie@yahoo.com>
2024-02-05 21:57:59 +0100

    header mis à jour avec celui de kasa

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-
+import "./header.scss";
 import { NavLink } from "react-router-dom";
+import logoForHeader from "../../assets/images/LOGO_header.png";
 
 function Header() {
   return (
-    <nav className="Header">
+    <nav className="header">
+      <div className="header_logo">
+        <img
+          src={logoForHeader}
+          alt="logo rouge rosé intitulé KASA avec une icone de maison à la place de la première lettre a"
+        />
+      </div>
       <NavLink to="/">
         <div>Accueil</div>
+      </NavLink>
+      <NavLink to="/about">
+        <div>A Propos</div>
       </NavLink>
     </nav>
   );
