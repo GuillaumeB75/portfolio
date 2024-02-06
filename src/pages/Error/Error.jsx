7e213e23c17b44b7c538b7b146b624e1e5764155
GuillaumeB75 <guillaume.bezie@yahoo.com>
2024-02-06 02:17:10 +0100

    mise en place page erreur

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./error.scss";
+import { NavLink } from "react-router-dom";
+
+function Error() {
+  return (
+    <div className="error_page">
+      <h1>404</h1>
+      <h2>Sorry, this page doesn't exist.</h2>
+      <NavLink to="/">
+        <h3>Come back to home</h3>
+      </NavLink>
+    </div>
+  );
+}
+
+export default Error;
