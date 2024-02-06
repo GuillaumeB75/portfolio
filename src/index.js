5a9fed9f94843f0b172a16019763fe5e4f999991
GuillaumeB75 <guillaume.bezie@yahoo.com>
2024-02-06 10:45:55 +0100

    correction redirections et mise en forme prettier

--- src/index.js
+++ src/index.js
@@ -6,14 +6,12 @@ import RoutesDefinition from "./routes/RoutesConfiguration";
 import reportWebVitals from "./reportWebVitals";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import About from "./components/About/About";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-    <Header />
-    <About />
+      <Header />
       <RoutesDefinition />
       <Footer />
     </Router>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
-
+import "./home.scss";
+import About from "../../components/About/About";
 
 function Home() {
   return (
     <div className="home_presentation">
-     
+      <About />
     </div>
   );
 }
