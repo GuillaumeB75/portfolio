107748afd98cbbcbf72a41fff87698630d46a56f
GuillaumeB75 <guillaume.bezie@yahoo.com>
2024-02-08 01:46:13 +0100

    mise en forme prettier

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import "./header.scss";
 
-import { Link } from 'react-scroll';
-
-
+import { Link } from "react-scroll";
 
 function Header() {
   return (
     <nav className="header">
-      
-      
-      
-      <Link to="Start" smooth={true} offset={-70} duration={1000}>Home</Link>
-      <Link to="about" smooth={true} duration={1000}>About</Link>
-      <Link to="Skills" smooth={true} offset={-70} duration={1000}>Skills</Link>
-      <Link to="Projects" smooth={true} offset={-70} duration={1000}>Projects</Link>
-      <Link to="Contact" smooth={true} offset={-70} duration={1000}>Contact</Link>
-
-      
-
+      <Link to="Start" smooth={true} offset={-70} duration={1000}>
+        Home
+      </Link>
+      <Link to="about" smooth={true} duration={1000}>
+        About
+      </Link>
+      <Link to="Skills" smooth={true} offset={-70} duration={1000}>
+        Skills
+      </Link>
+      <Link to="Projects" smooth={true} offset={-70} duration={1000}>
+        Projects
+      </Link>
+      <Link to="Contact" smooth={true} offset={-70} duration={1000}>
+        Contact
+      </Link>
     </nav>
   );
 }
