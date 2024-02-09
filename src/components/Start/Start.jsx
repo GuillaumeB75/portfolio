faeaf9c019b6e3e700ecd405931abe5d41ce787a
GuillaumeB75 <guillaume.bezie@yahoo.com>
2024-02-09 16:16:37 +0100

    aménagement effet d'animation texte

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -1,20 +1,56 @@
-import React from "react";
+import React, { useEffect, useRef } from 'react';
 import { Element } from "react-scroll";
 import "./start.scss";
 import back from "../../assets/images/back.jpg";
 
 function Start() {
+  // Utilisez useRef pour obtenir des références aux éléments DOM que vous allez animer
+  const h1Ref = useRef(null);
+  const h2Ref = useRef(null);
+
+  // Définissez la vitesse de l'effet de frappe
+  const speed = 100;
+
+  // Fonction pour appliquer l'effet de frappe
+  const typeEffect = (element, speed) => {
+    const text = element.innerHTML;
+    element.innerHTML = "";
+
+    let i = 0;
+    const timer = setInterval(() => {
+      if (i < text.length) {
+        element.append(text.charAt(i));
+        i++;
+      } else {
+        clearInterval(timer);
+      }
+    }, speed);
+  };
+
+  // Utilisez useEffect pour exécuter l'animation après le montage du composant
+  useEffect(() => {
+    if (h1Ref.current && h2Ref.current) {
+      typeEffect(h1Ref.current, speed);
+      const delay = h1Ref.current.innerHTML.length * speed + speed;
+
+      setTimeout(() => {
+        h2Ref.current.style.display = "inline-block";
+        typeEffect(h2Ref.current, speed);
+      }, delay);
+    }
+  }, [speed]);
+
   return (
     <Element
       name="Start"
       className="start-container"
-      style={{ background: `url(${back})` }}
+      style={{ backgroundImage: `url(${back})` }}
     >
-      <div className="bio">
-        <h1>Bonjour !</h1>
-        <h1>Je suis Guillaume Bézie</h1>
-        <h1>développeur intégrateur web</h1>
-        <h1>et futur consultant AMOA</h1>
+      <div id="table">
+        <div id="centeralign">
+          <h1 ref={h1Ref}>Bonjour !</h1>
+          <h2 ref={h2Ref} style={{ display: 'none' }}>Je suis Guillaume Bézie, développeur intégrateur Web</h2>
+        </div>
       </div>
     </Element>
   );
