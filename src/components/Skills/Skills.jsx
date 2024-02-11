351dc53cb7626917cfcc9ac000798bcb8ac94777
GuillaumeB75 <guillaume.bezie@yahoo.com>
2024-02-11 03:08:26 +0100

    ajout image fichier skills et correction css du même componant pour l'alignement en colonne lors du responsive

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,31 +1,84 @@
-import React from 'react';
+import React from "react";
 import { Element } from "react-scroll";
-import './Skills.scss';
+import "./Skills.scss";
+
+// Import des images
+import javascriptImg from "../../assets/images/javascript.png"; // Mettez à jour le chemin
+import reactImg from "../../assets/images/react.png"; // Mettez à jour le chemin
+import sassImg from "../../assets/images/sass.png"; // Mettez à jour le chemin
+import nodeImg from "../../assets/images/NodeJS.png"; // Mettez à jour le chemin
+import mongoImg from "../../assets/images/MongoDB.png"; // Mettez à jour le chemin
+import metamaskImg from "../../assets/images/MetaMask.png";
+import jiraImg from "../../assets/images/Jira.jpeg"; // Mettez à jour le chemin
+import htmlImg from "../../assets/images/HTML5.png"; // Mettez à jour le chemin
+import hardhatImg from "../../assets/images/Hardhat.png"; // Mettez à jour le chemin
+import githubImg from "../../assets/images/Github.png"; // Mettez à jour le chemin
+import expressImg from "../../assets/images/ExpressJS.png";
+import etherImg from "../../assets/images/EtherJS.png"; // Mettez à jour le chemin
+import cssImg from "../../assets/images/CSS3.png"; // Mettez à jour le chemin
+import bootstrapImg from "../../assets/images/Bootstrap.jpeg"; // Mettez à jour le chemin
+import solidityImg from "../../assets/images/Solidity.png"; // Mettez à jour le chemin
+
+// Répétez pour les autres technologies
 
 function Skills() {
-  
   return (
     <Element name="Skills" className="skills-container">
-      <h2>Compétences</h2>
-      <div>
+      <div className="card">
         <ul>
-          <li><i className="fa-brands fa-js fa-xl"></i> Javascript</li>
-          <li><i className="fas fa-code"></i> REACT.JS</li>
-          <li><i className="fas fa-code"></i> SASS</li>
+          <li>
+            <img src={javascriptImg} alt="Javascript" /> Javascript
+          </li>
+          <li>
+            <img src={reactImg} alt="REACT.JS" /> REACT.JS
+          </li>
+          <li>
+            <img src={sassImg} alt="SASS" /> SASS
+          </li>
+          <li>
+            <img src={reactImg} alt="REACT.JS" /> REACT.JS
+          </li>
+          <li>
+            <img src={sassImg} alt="SASS" /> SASS
+          </li>
         </ul>
       </div>
-      <div>
+      <div className="card">
         <ul>
-          <li><i className="fas fa-database"></i> Node.JS</li>
-          <li><i className="fas fa-database"></i> MongoDB</li>
-          <li><i className="fas fa-database"></i> Epress.JS</li>
+          <li>
+            <img src={javascriptImg} alt="Javascript" /> Javascript
+          </li>
+          <li>
+            <img src={reactImg} alt="REACT.JS" /> REACT.JS
+          </li>
+          <li>
+            <img src={sassImg} alt="SASS" /> SASS
+          </li>
+          <li>
+            <img src={reactImg} alt="REACT.JS" /> REACT.JS
+          </li>
+          <li>
+            <img src={sassImg} alt="SASS" /> SASS
+          </li>
         </ul>
       </div>
-      <div>
+      <div className="card">
         <ul>
-          <li><i className="fas fa-cloud"></i> Bootstrap</li>
-          <li><i className="fas fa-cloud"></i> HTML5 et CSS3</li>
-          <li><i className="fas fa-cloud"></i> Github</li>
+          <li>
+            <img src={javascriptImg} alt="Javascript" /> Javascript
+          </li>
+          <li>
+            <img src={reactImg} alt="REACT.JS" /> REACT.JS
+          </li>
+          <li>
+            <img src={sassImg} alt="SASS" /> SASS
+          </li>
+          <li>
+            <img src={reactImg} alt="REACT.JS" /> REACT.JS
+          </li>
+          <li>
+            <img src={sassImg} alt="SASS" /> SASS
+          </li>
         </ul>
       </div>
     </Element>
