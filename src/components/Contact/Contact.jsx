fe4fcae05eb5f67fe591cf5ccce2bf8afc9ec0e8
GuillaumeB75 <guillaume.bezie@yahoo.com>
2024-02-12 16:21:11 +0100

    modification cards

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,24 +3,30 @@ import { Element } from "react-scroll";
 import "./contact.scss";
 import backgroundContact from "../../assets/images/background.jpg";
 
-
 function Contact() {
   const email = "guillaume.bezie@yahoo.com";
   const phoneNumber = "+33 6 66 82 63 16";
 
   return (
-    <Element name="Contact" className="contact-info" style={{ backgroundImage: `url(${backgroundContact})` }}>
+    <Element
+      name="Contact"
+      className="contact-info"
+      style={{ backgroundImage: `url(${backgroundContact})` }}
+    >
       <div>
-      <p>
-        <i className="fas fa-phone"></i> {phoneNumber}
-      </p>
-      <p>
-        <i className="fas fa-envelope"></i>
-        <a href={`mailto:${email}`} title="Envoyer un email">
-          {email}
-        </a>
-      </p>
-    </div>
+        <h2 
+          className="title-contact">Me contacter :
+        </h2>
+        <p>
+          <i className="fas fa-phone"></i> {phoneNumber}
+        </p>
+        <p>
+          <i className="fas fa-envelope"></i>
+          <a href={`mailto:${email}`} title="Envoyer un email">
+            {email}
+          </a>
+        </p>
+      </div>
     </Element>
   );
 }
