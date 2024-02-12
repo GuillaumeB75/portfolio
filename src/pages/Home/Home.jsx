d4c87fbd07352faaa6475fa553c4c70ff4206310
GuillaumeB75 <guillaume.bezie@yahoo.com>
2024-02-12 20:26:08 +0100

    correction

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./home.scss";
 import Start from "../../components/Start/Start";
 import About from "../../components/About/About";
-import Header from "../../components/Header/Header";
+import Header from "../../components/Header/header";
 import Footer from "../../components/Footer/Footer";
 import Skills from "../../components/Skills/Skills";
 import Cards from "../../components/Projects/Projects";
