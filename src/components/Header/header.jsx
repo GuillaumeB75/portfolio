1ee62b86389f7567b6adccf8eaec794182255240
GuillaumeB75 <guillaume.bezie@yahoo.com>
2024-02-16 23:17:38 +0100

    amélioration référencement header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -6,19 +6,19 @@ import { Link } from "react-scroll";
 function Header() {
   return (
     <nav className="header">
-      <Link to="Start" smooth={true} offset={-70} duration={1000}>
+      <Link to="Start" smooth={true} offset={-70} duration={1000} aria-label="Naviguer vers la section d'accueil">
         Accueil
       </Link>
-      <Link to="about" smooth={true} duration={1000}>
+      <Link to="about" smooth={true} offset={-70} duration={1000} aria-label="Naviguer vers la section à propos">
         À propos
       </Link>
-      <Link to="Skills" smooth={true} offset={-70} duration={1000}>
+      <Link to="Skills" smooth={true} offset={-70} duration={1000} aria-label="Naviguer vers la section compétences">
         Compétences
       </Link>
-      <Link to="Projects" smooth={true} offset={-70} duration={1000}>
+      <Link to="Projects" smooth={true} offset={-70} duration={1000} aria-label="Naviguer vers la section projets">
         Projets
       </Link>
-      <Link to="Contact" smooth={true} offset={-70} duration={1000}>
+      <Link to="Contact" smooth={true} offset={-70} duration={1000} aria-label="Naviguer vers la section contact">
         Contact
       </Link>
     </nav>
